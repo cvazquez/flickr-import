493a2188175a2cd8f3c28992bea4cda0c566dca3
Carlos Vazquez <cvazquez1976@gmail.com>
2020-05-23 17:30:37 -0400

    Move api functions to a new component

---
+++ components/api.js
@@ -0,0 +1,127 @@
+const https	= require("https");
+
+exports.api = class api {
+	constructor(flickrModel) {
+		this.flickrModel = flickrModel;
+
+		return (async () => {
+			this.api = await this.getAPICreds();
+
+			return this;
+		})();
+	}
+
+	async getAPICreds() {
+		const apiCreds	= await this.flickrModel.getCredentials();
+		let api = {
+			key		: null,
+			secret	: null,
+			userId	: null
+		};
+
+		apiCreds.map(cred => {
+			switch(cred.name) {
+				case "flickrAPIKey":
+					api.key = cred.value;
+				break;
+				case "flikrAPISecret":
+					api.secret = cred.value;
+				break;
+				case "flickrUserId":
+					api.userId = cred.value;
+				break;
+			}
+		});
+
+		return api;
+	}
+
+	getCollections(collectionId) {
+		return new Promise((resolve, reject) => {
+			https.get(`https://api.flickr.com/services/rest/?method=flickr.collections.getTree&api_key=${this.api.key}&collection_id=${collectionId}&user_id=${this.api.userId}&format=json&nojsoncallback=1`,
+				res => {
+					var body = "";
+
+					res.on("data", data => {
+						body += data;
+					});
+
+					res.on("end", () => {
+						if(res.statusCode === 200) {
+							try {
+								let jsonResponse = JSON.parse(body);
+
+								if(!jsonResponse.stat || jsonResponse.stat !== "ok" ||
+									!jsonResponse.collections || !jsonResponse.collections.collection ||
+									!jsonResponse.collections.collection[0] || !jsonResponse.collections.collection[0].set)
+									throw new Error("Error parsing JSON");
+
+								process.stdout.write(`************* COLLECTION TITLE: ${jsonResponse.collections.collection[0].title} ************\n`);
+
+								resolve({set : jsonResponse.collections.collection[0].set});
+							} catch(e) {
+
+								process.stdout.write(`Error parsing JSON from getCollections(${collectionId})`);
+								console.log(e);
+								reject(e);
+							}
+						} else {
+							throw new Error(`Bad response code in getAPICollections(${collectionId})`);
+						}
+					});
+			}).on('error', (e) => {
+				process.stdout.write(`\ngetAPICollections(${collectionId}) API response error\n`);
+				console.error(e);
+
+				reject(e);
+			});
+		}).catch(err => {
+			process.stdout.write(`\ngetAPICollections(${collectionId}) Promise Error\n`);
+			process.stdout.write(err);
+		});
+	}
+
+	async getAlbumPhotos(albumId) {
+
+		process.stdout.write(`\n\n********** API get Album photos for ${albumId} **********\n`);
+
+		return new Promise((resolve, reject) => {
+			https.get(`https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${this.api.key}&photoset_id=${albumId}&extras=description,date_taken,url_sq,url_s,url_m,url_o,geo,tags&privacy_filter=1&user_id=${this.api.userId}&per_page=100&format=json&nojsoncallback=1`,
+					(res) => {
+						var body = '';
+
+						res.on('data', function(chunk){
+							body += chunk;
+						});
+
+						res.on("end", () => {
+							if(res.statusCode === 200) {
+								try {
+									const jsonResponse = JSON.parse(body);
+
+									if(!jsonResponse.stat || jsonResponse.stat !== "ok" || !jsonResponse.photoset) {
+										throw new Error("jsonResponse bad");
+									}
+
+									resolve({set : jsonResponse.photoset});
+
+								} catch(e) {
+									process.stdout.write(`\nError parsing JSON from getPhotoAlbums(${albumId})\n`);
+									process.stdout.write(e)
+								}
+							} else {
+								process.stdout.write(`\ngetPhotoAlbums(${albumId}) bad status code (${res.statusCode})\n`);
+							}
+						});
+					}).on('error', (e) => {
+						process.stdout.write(`\ngetPhotoAlbums(${albumId}) API response error\n`);
+						console.error(e);
+
+						reject(e);
+					});
+		}).catch(err => {
+			process.stdout.write(`\ngetPhotoAlbums(${albumId}) Promise Error\n`);
+			process.stdout.write(err);
+		});
+	}
+}
--- flickr.js
+++ flickr.js
@@ -1,85 +1,20 @@
 const	flikrDS			= require('./connectors/mysql').connection,
 		flickrObject	= require("./models/flickr").flickrModel,
 		flickrModel		= new flickrObject(flikrDS),
-		https			= require("https");
+		apiClass		= require('./components/api').api;
 
-let		api	=	{
-			key		: null,
-			secret	: null,
-			userId	: null
-		};
-
-async function setAPICreds() {
-	const apiCreds		= await flickrModel.getCredentials();
-
-	apiCreds.map(cred => {
-		switch(cred.name) {
-			case "flickrAPIKey":
-				api.key = cred.value;
-			break;
-			case "flikrAPISecret":
-				api.secret = cred.value;
-			break;
-			case "flickrUserId":
-				api.userId = cred.value;
-			break;
-		}
-	});
-}
+let api;
 
 async function processCollections(api) {
 	const	collections = await flickrModel.getCollections();
 
-	await collections.map(async (collection, index) => {
+	 collections.map(async (collection, index) => {
 		process.stdout.write(`\n************* COLLECTION ID: ${collection.id} ************\n`);
-		process.stdout.write("collections index = ", index)
-		await getAPICollections(collection.id);
-	});
-}
 
-function getAPICollections(collectionId) {
-	return new Promise((resolve, reject) => {
-		https.get(`https://api.flickr.com/services/rest/?method=flickr.collections.getTree&api_key=${api.key}&collection_id=${collectionId}&user_id=${api.userId}&format=json&nojsoncallback=1`,
-			res => {
-				var body = "";
+		let collectionAPI = await api.getCollections(collection.id);
 
-				res.on("data", data => {
-					body += data;
-				});
+		processAlbums(collectionAPI.set, collection.id);
 
-				res.on("end", () => {
-					if(res.statusCode === 200) {
-						try {
-							let jsonResponse = JSON.parse(body);
-
-							if(!jsonResponse.stat || jsonResponse.stat !== "ok" ||
-								!jsonResponse.collections || !jsonResponse.collections.collection ||
-								!jsonResponse.collections.collection[0] || !jsonResponse.collections.collection[0].set)
-								throw new Error("Error parsing JSON");
-
-							process.stdout.write(`************* COLLECTION TITLE: ${jsonResponse.collections.collection[0].title} ************\n`);
-							resolve({
-										process : processAlbums(jsonResponse.collections.collection[0].set, collectionId),
-										success	: true});
-						} catch(e) {
-
-							process.stdout.write(`Error parsing JSON from getAPICollections(${collectionId})`);
-							process.stdout.write(e)
-							reject(e)
-						}
-					} else {
-						throw new Error(`Bad response code in getAPICollections(${collectionId})`);
-					}
-				});
-		}).on('error', (e) => {
-			process.stdout.write(`\ngetAPICollections(${collectionId}) API response error\n`);
-			console.error(e);
-
-			reject(e);
-		});
-	}).catch(err => {
-		process.stdout.write(`\ngetAPICollections(${collectionId}) Promise Error\n`);
-		process.stdout.write(err);
 	});
 }
 
@@ -88,7 +23,7 @@ async function processAlbums(albumbsFromFlikr, collectionId) {
 	let	albumIdsFromFlickr = [],
 		albumbsFromFlikrById = {},
 		albumbsFromDBById = {},
-		status;
+		album;
 
 	// Turn albums from Flicker API call into an array and object of Flickr albums
 	[albumIdsFromFlickr, albumbsFromFlikrById] = await getAlbumnsByIdObjects(albumbsFromFlikr);
@@ -100,10 +35,11 @@ async function processAlbums(albumbsFromFlikr, collectionId) {
 	await CheckAndCreateMissingDatabaseAlbums(albumbsFromFlikrById, albumbsFromDBById, collectionId);
 
 	for(let albumId in albumbsFromDBById) {
-		status = await getAlbumPhotos(albumId);
+		album = await api.getAlbumPhotos(albumId);
+		processPhotos(album.set, albumId)
 	}
 
-	return await status;
+//	return await status;
 }
 
 // Return an array and object by id to access Flick album data
@@ -212,51 +148,6 @@ async function CheckAndCreateMissingDatabaseAlbums(albumbsFromFlikrById, albumbs
 	return;
 }
 
-async function getAlbumPhotos(albumId) {
-
-	process.stdout.write(`\n\n********** API get Album photos for ${albumId} **********\n`);
-
-	return new Promise((resolve, reject) => {
-		https.get(`https://api.flickr.com/services/rest/?method=flickr.photosets.getPhotos&api_key=${api.key}&photoset_id=${albumId}&extras=description,date_taken,url_sq,url_s,url_m,url_o,geo,tags&privacy_filter=1&user_id=${api.userId}&per_page=100&format=json&nojsoncallback=1`,
-				(res) => {
-					var body = '';
-
-					res.on('data', function(chunk){
-						body += chunk;
-					});
-
-					res.on("end", () => {
-						if(res.statusCode === 200) {
-							try {
-								const jsonResponse = JSON.parse(body);
-
-								if(!jsonResponse.stat || jsonResponse.stat !== "ok" || !jsonResponse.photoset) {
-									throw new Error("jsonResponse bad");
-								}
-
-								resolve({
-									process : processPhotos(jsonResponse.photoset, albumId),
-									success	: true});
-							} catch(e) {
-								process.stdout.write(`\nError parsing JSON from getPhotoAlbums(${albumId})\n`);
-								process.stdout.write(e)
-							}
-						} else {
-							process.stdout.write(`\ngetPhotoAlbums(${albumId}) bad status code (${res.statusCode})\n`);
-						}
-					});
-				}).on('error', (e) => {
-					process.stdout.write(`\ngetPhotoAlbums(${albumId}) API response error\n`);
-					console.error(e);
-
-					reject(e);
-				});
-	}).catch(err => {
-		process.stdout.write(`\ngetPhotoAlbums(${albumId}) Promise Error\n`);
-		process.stdout.write(err);
-	});
-}
-
 async function processPhotos(flickrPhotos, albumId) {
 	let photoValues = [], // hold an array of sql insert values
 		result,
@@ -345,11 +236,11 @@ async function processPhotos(flickrPhotos, albumId) {
 	return true;
 }
 
-setAPICreds();
-//processCollections(api);
-
 let myPromise = new Promise(async (resolve, reject) => {
+	api = await new apiClass(flickrModel)
+
 	await processCollections(api);
+
 	resolve("success")
 })
 
