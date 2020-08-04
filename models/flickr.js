52709f9b75592566bb1e9c759a886ea2d474d3e1
Carlos Vazquez <cvazquez1976@gmail.com>
2020-08-04 14:45:45 -0400

    * Add a parent model with a promise and query, and pass queries through it

--- models/flickr.js
+++ models/flickr.js
@@ -1,196 +1,122 @@
-exports.flickrModel = class flickrModel {
+const model	= require("../models/model");
+
+exports.flickrModel = class flickrModel extends model {
 	constructor(ds) {
+		super(ds);
 		this.ds = ds;
 	}
 
 	getAlbumInfo(albumbIds) {
-		return new Promise((resolve, reject) => {
-		this.ds.query(`	SELECT id, title, description
-						FROM flickrsets
-						WHERE	id IN (${albumbIds})
-								AND deletedAt IS NULL`,
-			(err, rows) => {
-				if(err) console.log(err);
-
-				resolve(rows);
-			}
-		)})
+		return this.getQueryResults(
+			"getAlbumInfo",
+			`	SELECT id, title, description
+				FROM flickrsets
+				WHERE	id IN (${albumbIds})
+						AND deletedAt IS NULL`
+		)
 	}
 
 	getAlbumPhotos(albumId) {
-		return new Promise((resolve, reject) => {
-			this.ds.query(`	SELECT id, title, description
-							FROM flickrsetphotos
-							WHERE	flickrSetId = ?
-									AND deletedAt IS NULL`, [albumId],
-				(err, rows) => {
-					if(err) reject(err);
-
-					resolve(rows);
-				})
-		}).catch(err => {
-			return({failed: true,
-					reason: err});
-		})
+		return this.getQueryResults(
+			"getAlbumPhotos",
+			`	SELECT id, title, description
+				FROM flickrsetphotos
+				WHERE	flickrSetId = ?
+						AND deletedAt IS NULL`, [albumId]
+		)
 	}
 
 	getCollections() {
-		return new Promise((resolve, reject) => {
-			this.ds.query(`
-				SELECT id
+		return this.getQueryResults(
+			"getCollections",
+			`	SELECT id
 				FROM	flickrcollections
-				WHERE	deletedAt IS NULL;`,
-			(err, rows) => {
-				if(err) console.log(err);
-
-				resolve(rows);
-			})
-		})
+				WHERE	deletedAt IS NULL;`
+		)
 	}
 
 	getCredentials() {
-		return new Promise((resolve, reject) => {
-			this.ds.query(`	SELECT name, value
-							FROM adminsettings
-							WHERE	name IN ('flickrAPIKey', 'flikrAPISecret', 'flickrUserId')
-									AND deletedAt IS NULL`,
-			(err, rows) => {
-				if(err) console.log(err);
-
-				resolve(rows);
-			})
-		})
+		return this.getQueryResults(
+			"getCredentials",
+			`	SELECT name, value
+				FROM adminsettings
+				WHERE	name IN ('flickrAPIKey', 'flikrAPISecret', 'flickrUserId')
+						AND deletedAt IS NULL`
+		)
 	}
 
 	getOAuthAccessToken(userName) {
-		return new Promise((resolve, reject) => {
-			this.ds.query(`	SELECT userNSid, fullName, oauthToken, oauthTokenSecret
-							FROM flickroauth
-							WHERE 	userName = ?
-									AND deletedAt IS NULL
-							ORDER BY id desc
-							LIMIT 1`, [userName],
-				(err, rows) => {
-					if(err) reject(err);
-
-					resolve(rows);
-				});
-		}).catch(err => {
-			return({failed: true,
-					reason: err});
-		});
+		return this.getQueryResults(
+			"getOAuthAccessToken",
+			`	SELECT userNSid, fullName, oauthToken, oauthTokenSecret
+				FROM flickroauth
+				WHERE 	userName = ?
+						AND deletedAt IS NULL
+				ORDER BY id desc
+				LIMIT 1`, [userName]
+		)
 	}
 
 	saveAlbum(id, flickrCollectionId, title, description) {
-		return new Promise((resolve, reject) => {
-			this.ds.query(`	INSERT INTO flickrsets
-							SET id 					= ?,
-								flickrCollectionId	= ?,
-								title				= ?,
-								description			= ?,
-								createdAt			= now()`,
-							[id, flickrCollectionId, title, description],
-							(err, rows) => {
-								if(err) reject(err);
-
-								resolve(rows);
-							})
-		}).catch((err) => {
-			let reason = err;
-
-			if(err.errno === 1062) {
-				reason = "Photo Albums already exist in DB";
-			}
-
-			return({failed: true,
-					reason: reason});
-		});
+		return this.getQueryResults(
+			"saveAlbum",
+			`	INSERT INTO flickrsets
+				SET id 					= ?,
+					flickrCollectionId	= ?,
+					title				= ?,
+					description			= ?,
+					createdAt			= now()`,
+				[id, flickrCollectionId, title, description]
+		)
 	}
 
 	saveOAuth(userNSid, fullName, userName, oauthToken, oauthTokenSecret) {
-		return new Promise((resolve, reject) => {
-			this.ds.query(`	INSERT INTO flickroauth (userNSid, fullName, userName, oauthToken, oauthTokenSecret)
-							VALUES (?, ?, ?, ?, ?)`, [userNSid, fullName, userName, oauthToken, oauthTokenSecret],
-					(err, rows) => {
-						if(err) reject(err);
-
-						resolve(rows);
-					})
-		}).catch(err => {
-			return({failed: true,
-					reason: err});
-		})
+		return this.getQueryResults(
+			"saveOAuth",
+			`	INSERT INTO flickroauth (userNSid, fullName, userName, oauthToken, oauthTokenSecret)
+				VALUES (?, ?, ?, ?, ?)`, [userNSid, fullName, userName, oauthToken, oauthTokenSecret]
+		)
 	}
 
 	savePhotos(values) {
-		return new Promise((resolve, reject) => {
-			this.ds.query(`	INSERT IGNORE INTO flickrsetphotos
-							(	id, flickrSetId, orderId,
-								title, description,
-								smallURL, smallWidth, smallHeight,
-								squareURL, squareWidth, squareHeight,
-								mediumURL, mediumWidth, mediumHeight,
-								largeURL, largeWidth, largeHeight,
-								takenAt)
-							VALUES ?;`,
-							[values],
-					(err, rows) => {
-						if(err) reject(err);
-
-						resolve(rows)
-					})
-		}).catch((err) => {
-			let reason = err;
-
-			if(err.errno === 1062) {
-				reason = "Photos already exist in DB";
-			} else {
-			//	console.log(err);
-			}
-
-			return({failed: true,
-					reason: reason,
-					errno: err.errno});
-		});
+		return this.getQueryResults(
+			"savePhotos",
+			`	INSERT IGNORE INTO flickrsetphotos
+				(	id, flickrSetId, orderId,
+					title, description,
+					smallURL, smallWidth, smallHeight,
+					squareURL, squareWidth, squareHeight,
+					mediumURL, mediumWidth, mediumHeight,
+					largeURL, largeWidth, largeHeight,
+					takenAt)
+				VALUES ?;`,
+				[values]
+		)
 	}
 
 	savePhotoURLS() {
-		return new Promise((resolve, reject) => {
-			this.ds.query(`	INSERT IGNORE INTO flickrsetphotourls (flickrSetPhotoId, name, isActive, createdAt)
-							SELECT id, CreateTitleURL(title), 1, now()
-							FROM flickrsetphotos
-							WHERE deletedAt IS NULL
-							ORDER BY id;`,
-				(err, rows) => {
-					if(err) reject(err);
-
-					resolve(rows);
-				})
-		}).catch((err) => {
-			return({failed: true,
-					reason: err});
-		});
+		return this.getQueryResults(
+			"savePhotoURLS",
+			`	INSERT IGNORE INTO flickrsetphotourls (flickrSetPhotoId, name, isActive, createdAt)
+				SELECT id, CreateTitleURL(title), 1, now()
+				FROM flickrsetphotos
+				WHERE deletedAt IS NULL
+				ORDER BY id;`
+		)
 	}
 
 	updateAlbum(id,
 				title,
 				description) {
-
-		return new Promise((resolve, reject) => {
-			this.ds.query(`	UPDATE flickrsets
-							SET title = ?,
-								description	= ?,
-								updatedAt = CASE WHEN (? <> title OR ? <> description) THEN now() ELSE updatedAt END
-							WHERE id = ?;`,
-							[title, description, title, description, id],
-				(err, rows) => {
-					if(err) reject(err);
-
-					resolve(rows);
-				})
-			}).catch((err) => {
-				console.log(`updateAlbum(${albumId} error : )`, err);
-			});
+		return this.getQueryResults(
+			"updateAlbum",
+			`	UPDATE flickrsets
+				SET title = ?,
+					description	= ?,
+					updatedAt = CASE WHEN (? <> title OR ? <> description) THEN now() ELSE updatedAt END
+				WHERE id = ?;`,
+				[title, description, title, description, id]
+		)
 	}
 
 	updatePhoto(id,
@@ -208,8 +134,9 @@ exports.flickrModel = class flickrModel {
 				largeURL,
 				largeWidth,
 				largeHeight) {
-		return new Promise((resolve, reject) => {
-			this.ds.query(`	UPDATE flickrsetphotos
+		return this.getQueryResults(
+			"updatePhoto",
+			`	UPDATE flickrsetphotos
 							SET	title		= ?,
 								description	= ?,
 								squareURL	= ?,
@@ -239,14 +166,7 @@ exports.flickrModel = class flickrModel {
 											largeURL,
 											largeWidth,
 											largeHeight,
-											id],
-				(err, rows) => {
-					if(err) reject(err);
-
-					resolve(rows);
-				})
-		}).catch(err => {
-			console.log(`updatePhoto(${photoId}, ${title}, ${description})`, err);
-		})
+											id]
+		)
 	}
 }
